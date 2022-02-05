3706d9a2521deed663ffb4d7cc77c8d7a32ed25e
miyuki-noguchi <miy0916uki@gmail.com>
2022-02-05 13:36:31 +0900

    スキル円グラフ追加

--- 1bebc8a8492271336d27bc7d11cd69922204d84c84c8fe43b190b232c84c60de/js/index.js
+++ 1bebc8a8492271336d27bc7d11cd69922204d84c84c8fe43b190b232c84c60de/js/index.js
@@ -27,17 +27,6 @@ $(function () {
 
   $('.mvTxt').fadeIn(3000);
 
-  // profile
-  // const targetAnime = $('#profile');
-  // const twitter = $('.twitter');
-  // const elAnime = targetAnime.offset().top;
-  // $(window).scroll(function () {
-  //   const scroll = $(this).scrollTop();
-  //   if (scroll > elAnime) {
-  //     twitter.addClass('');
-  //   }
-  // });
-
   $('.tab > div:not(' + $('a.selected').attr('href') + ')').hide();
 
   $('#tabPages a').click(function () {
@@ -46,11 +35,6 @@ $(function () {
     $('.work_page').hide();
     $($(this).attr('href')).show();
     return false;
-
-    // const selectedWork = $(this).data('image');
-    // if ($('.work_icon_img').hasClass('selectedWork')) {
-    //   this.addClass('scale');
-    // }
   });
 
   // twenty
@@ -72,11 +56,95 @@ $(function () {
     });
   });
 
-  $('#aa').click(function () {
-    alert('re');
+  // skillグラフ
+  //値をグラフに表示させる
+  Chart.plugins.register({
+    afterDatasetsDraw: function (chart, easing) {
+      var ctx = chart.ctx;
+
+      chart.data.datasets.forEach(function (dataset, i) {
+        var meta = chart.getDatasetMeta(i);
+        if (!meta.hidden) {
+          meta.data.forEach(function (element, index) {
+            // 値の表示
+            ctx.fillStyle = 'rgb(77, 72, 84,1)'; //文字の色
+            var fontSize = 16; //フォントサイズ
+            var fontStyle = 'normal'; //フォントスタイル
+            var fontFamily = 'Arial'; //フォントファミリー
+            ctx.font = Chart.helpers.fontString(
+              fontSize,
+              fontStyle,
+              fontFamily
+            );
+
+            var dataString = dataset.data[index].toString();
+
+            // 値の位置
+            ctx.textAlign = 'center'; //テキストを中央寄せ
+            ctx.textBaseline = 'middle'; //テキストベースラインの位置を中央揃え
+
+            var padding = 5; //余白
+            var position = element.tooltipPosition();
+            ctx.fillText(
+              dataString,
+              position.x,
+              position.y - fontSize / 2 - padding
+            );
+          });
+        }
+      });
+    },
   });
 
-  // skill図
+  //=========== 円グラフ ============//
+  $('#chart01').on('inview', function (event, isInView) {
+    //画面上に入ったらグラフを描画
+    if (isInView) {
+      var ctx = document.getElementById('chart01'); //グラフを描画したい場所のid
+      var chart = new Chart(ctx, {
+        type: 'pie', //グラフのタイプ
+        data: {
+          //グラフのデータ
+          labels: ['Coading', 'Design', 'Direction'], //データの名前
+          datasets: [
+            {
+              label: '職種別比率', //グラフのタイトル
+              backgroundColor: ['#fb8877', '#9aa9ba', '#60a0f2'], //グラフの背景色
+              data: ['55', '35', '10'], //データ
+            },
+          ],
+        },
+
+        options: {
+          //グラフのオプション
+          maintainAspectRatio: false, //CSSで大きさを調整するため、自動縮小をさせない
+          legend: {
+            display: true, //グラフの説明を表示
+          },
+          tooltips: {
+            //グラフへカーソルを合わせた際の詳細表示の設定
+            callbacks: {
+              label: function (tooltipItem, data) {
+                return (
+                  data.labels[tooltipItem.index] +
+                  ': ' +
+                  data.datasets[0].data[tooltipItem.index] +
+                  '%'
+                ); //%を最後につける
+              },
+            },
+          },
+          title: {
+            //上部タイトル表示の設定
+            display: true,
+            fontSize: 16,
+            text: '単位：%',
+          },
+        },
+      });
+    }
+  });
+
   const target = $('#skill');
   const el = target.offset().top;
   $(window).scroll(function () {
